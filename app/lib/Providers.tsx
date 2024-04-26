4c227dd033a2a3af5a91e1e08c26e08e36b171c5
Noah Solomon <noahsolomon2003@gmail.com>
2024-04-25 21:22:33 -0400

    tooltip

--- app/lib/Providers.tsx
+++ app/lib/Providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { ThemeProvider } from 'next-themes'
 //@ts-ignore
 
@@ -10,10 +11,12 @@ interface ProvidersProps {
 const Providers = ({ children }: ProvidersProps) => {
   return (
     <ThemeProvider attribute='class'>
-      {/* <div className='fixed bottom-4 left-4 z-20 rounded-full border bg-primary/20'>
+      <TooltipProvider delayDuration={0}>
+        {/* <div className='fixed bottom-4 left-4 z-20 rounded-full border bg-primary/20'>
         {process.env.NEXT_PUBLIC_ENV === 'LOCAL' && <LagRadar />}
       </div> */}
-      {children}
+        {children}
+      </TooltipProvider>
     </ThemeProvider>
   )
 }
